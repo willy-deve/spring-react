d291a0935c5014d02d7ffc90bf8fae448330c3c3
willy <willydasilva29@gmail.com>
2022-11-19 19:53:03 -0300

    project clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import GlobalStyle from './config/GlobalStyles'
-import AppRoutes from './routes/AppRoutes'
-
-const temas = {
-  dark: {
-    color: '#f5f5f5',
-    backgroundColor: '#333',
-    primary: '#19def0',
-  },
-
-  light: {
-    color: '#333',
-    backgroundColor: '#f5f5f5',
-    primary: '#6747b6e9',
-  },
-}
 
 function App() {
-  return (
-    <ThemeProvider theme={temas.light}>
-      <GlobalStyle />
-      <AppRoutes />
-    </ThemeProvider>
-  )
+  return <h1>Salve</h1>
 }
 
 export default App
